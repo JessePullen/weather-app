8e627616267b35f740d41671672924bb9210e23e
JessePullen <48932462+JessePullen@users.noreply.github.com>
2024-09-24 13:36:18 +1000

    Initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,9 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+
+
+export default [
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+  {ignores: ["dist/main.js", "webpack.config.js"]}
+];
