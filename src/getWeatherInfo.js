61f51672f1d07da23a362f59ac8baa140dc5dc43
Jesse <jessepullen@icloud.com>
2024-09-26 13:43:27 +1000

    Add getWeatherInfo module

---
+++ src/getWeatherInfo.js
@@ -0,0 +1,29 @@
+let location = 'new york';
+
+async function getWeatherInfo() {
+	const url = addLocation(location);
+
+	const response = await fetch(url);
+	const data = await response.json();
+	console.log(data);
+}
+
+// Adds location to template url for API
+function addLocation(location) {
+	const templateUrl =
+		'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/?unitGroup=us&key=4PXUNQF6XEB3JUH9ASRHNWAYK&contentType=json';
+
+	const convertedLocation = convertLocationString(location);
+
+	const splitUrl = templateUrl.split('?');
+	const urlWithLocation = splitUrl[0] + convertedLocation + '?' + splitUrl[1];
+
+	return urlWithLocation;
+}
+
+// Converts inputted string to work in url for visualcrossing weather API
+function convertLocationString(location) {
+	return location.replace(' ', '%20');
+}
+
+export { getWeatherInfo };
--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
 import './style.css';
+import { getWeatherInfo } from './getWeatherInfo';
+
+getWeatherInfo();
