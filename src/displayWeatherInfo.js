ba43a8824a3aedde0e30c607d2c8f026cb6b42d8
Jesse <jessepullen@icloud.com>
2024-10-02 13:26:50 +1000

    Add function to convert F to C temperature units when searching for city

--- src/displayWeatherInfo.js
+++ src/displayWeatherInfo.js
@@ -1,12 +1,24 @@
 function displayWeatherInfo(data) {
 	const weatherInfoElements = document.querySelectorAll('.weather-info');
+	const temperatureUnitsCheckbox = document.querySelector('.temperature-units');
+
+	// Checked by default = celcius, unchecked = fahrenheit
+	if (temperatureUnitsCheckbox.checked) {
+		data.temperature = convertFahrenheitToCelcius(data.temperature);
+	}
 
 	weatherInfoElements[0].textContent = data.city;
-	weatherInfoElements[1].textContent = `Temperature: ${data.temperature}`;
+	weatherInfoElements[1].textContent = `Temperature: ${data.temperature} degrees`;
 	weatherInfoElements[2].textContent = `Conditions: ${data.conditions}`;
-	weatherInfoElements[3].textContent = `Precipitation chance: ${data.precipitatonChance}`;
-	weatherInfoElements[4].textContent = `Humidity: ${data.humidity}`;
+	weatherInfoElements[3].textContent = `Precipitation chance: ${data.precipitatonChance}%`;
+	weatherInfoElements[4].textContent = `Humidity: ${data.humidity}%`;
 	weatherInfoElements[5].textContent = `Wind speed: ${data.windSpeed} km/h`;
+}
+
+// Converts temperature from fahrenheit to elcius capped at 1 decimal place
+// This is done in this module to avoid calling the API again when the data is saved in object
+function convertFahrenheitToCelcius(temperature) {
+	return ((temperature - 32) / 1.8).toFixed(1);
 }
 
 export { displayWeatherInfo };
